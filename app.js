0268c118e3848830a64d083852bd69b21d8cb748
dag8 <dag8@pc2-023-l.cs.st-andrews.ac.uk>
2019-06-19 22:52:52 +0100

    implemented image uploading via multer

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 var express = require('express');
 var cors = require('cors')
 var bodyParser = require("body-parser");
-var app = express();
+var app = express(); 
+const multer = require('multer');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors())
 
-app.post('/',function(req,res){
-    console.log(req.body);
+// SET STORAGE
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+ 
+var upload = multer({ storage: storage })
+
+// ROUTES
+// app.get('/',function(req,res){
+//   res.sendFile(__dirname + '/index.html');
+ 
+// });
+
+app.post('/upload/photo', upload.single('myImage'), function(req,res, next){
+    const file = req.file;
+
+    if (!file) {
+      const error = new Error('Please upload a file')
+      error.httpStatusCode = 400
+      return next(error)
+    }
+      // res.send(file)
+
     res.setHeader('Content-Type', 'application/json');
     res.json({ "price": 20 });
   });
