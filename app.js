f1c05575d444cac2181688162cff5d9174360271
dag8 <dag8@pc2-023-l.cs.st-andrews.ac.uk>
2019-06-20 13:37:19 +0100

    got the python script to execute but having trouble accessing the values from that script

--- app.js
+++ app.js
@@ -3,13 +3,69 @@ var cors = require('cors')
 var bodyParser = require("body-parser");
 var app = express(); 
 const multer = require('multer');
-
+const spawn = require("child_process").spawn;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors())
 
-// SET STORAGE
+const logOutput = (name) => (data) => console.log(`[${name}] ${data}`)
+// Method for calling machine learning
+var processImage = () => {
+  let arg1 = 20;
+  //const pythonProcess = spawn('python',["./test.py", arg1]);
+  
+  return new Promise((resolve, reject) => {
+    const pythonProcess = spawn('python',["./test.py", arg1]);
+    
+    const out = []
+    pythonProcess.stdout.on("data", data =>{
+      out.push(data.toString());
+      logOutput('stdout')(data);
+      //console.log(data);
+    });
+
+    process.stderr.on("data", data => {
+      err.push(data.toString());
+      logOutput('stderr')(data);
+    });
+
+    process.on('exit', (code, signal) => {
+      logOutput('exit')(`${code} (${signal})`)
+      if (code === 0) {
+        resolve(out);
+      } else {
+        reject(new Error(err.join('\n')))
+      }
+    });
+  });
+}
+
+(async () => {
+  try {
+    const output = await processImage()
+    logOutput('main')(output)
+    process.exit(0)
+  } catch (e) {
+    console.error(e.stack);
+    process.exit(1);
+  }
+})();
+
+// var processImage = () => {
+//   let arg1 = 20;
+//   const pythonProcess = spawn('python',["./test.py", arg1]);
+//   //Listen for data from python script
+//   pythonProcess.stdout.on('data', (data) => {
+//     // Do something with the data returned from python script
+
+//     //console.log("logging data", data.toString())
+//     return data.toString()
+
+//   });
+// }
+
+// Set storage for pictures
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads')
@@ -22,11 +78,13 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage })
 
 // ROUTES
+// get request for testing
 // app.get('/',function(req,res){
 //   res.sendFile(__dirname + '/index.html');
  
 // });
 
+//post request to handle the inbound image
 app.post('/upload/photo', upload.single('myImage'), function(req,res, next){
     const file = req.file;
 
@@ -35,7 +93,9 @@ app.post('/upload/photo', upload.single('myImage'), function(req,res, next){
       error.httpStatusCode = 400
       return next(error)
     }
-      // res.send(file)
+
+    // Method for initiating analysis of the image
+    processImage();
 
     res.setHeader('Content-Type', 'application/json');
     res.json({ "price": 20 });
@@ -43,5 +103,9 @@ app.post('/upload/photo', upload.single('myImage'), function(req,res, next){
 
   app.listen(5000,function(){
     console.log("Started on PORT 5000");
+
+    //remove this 
+    var test = processImage(); 
+    console.log("testing again", test);
   })
   
---
+++ test.py
@@ -0,0 +1,6 @@
+import sys
+
+print("Python script is running")
+
+print(sys.argv[1])
+#sys.stdout.flush()
