11a33e750198206d285435d025223ca8001154fb
dag8 <dag8@pc5-018-l.cs.st-andrews.ac.uk>
2019-07-08 19:37:53 +0100

    built functionality to process the form input then dynamically assemble the regression data in the correct form

--- app.js
+++ app.js
@@ -9,15 +9,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors())
 
+app.listen(5000,function(){
+  console.log("Started on PORT 5000");
+})
+
+
 //Method for debugging the express application's interaction with the python script
 const logOutput = () => (data) => console.log(`${data}`)
 
 // Method for calling machine learning
-var processImage = () => {
-  let arg1 = 1;
+var processImage = (location, season) => {
+  let arg1 = processSeason(season);
+  let arg2 = processLocation(location);
   
   return new Promise((resolve, reject) => {
-    const pythonProcess = spawn('python',["./controller.py", arg1]);
+    const pythonProcess = spawn('python',["./controller.py", arg1, arg2]);
     
     const out = []
     pythonProcess.stdout.on("data", data =>{
@@ -42,6 +48,26 @@ var processImage = () => {
   });
 }
 
+var processSeason= (season) => {
+  switch(season) {
+    case "winter":
+      return [1,0,0,0]
+    case "spring":
+      return [0,1,0,0] 
+    case "summer":
+      return [0,0,1,0]
+    case "autumn":
+      return [0,0,0,1]
+    default:
+      return [0,0,1,0]
+  }
+}
+
+var processLocation = (location) => {
+  return (location === "Fife") ? 1 : 0; 
+}
+
+
 // Set storage for pictures
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -56,25 +82,20 @@ var upload = multer({ storage: storage })
 
 //post request to handle the inbound image and form data
 app.post('/upload/photo', upload.single('myImage'), function(req, res, next){
-    const file = req.file;
+  const file = req.file;
 
-    // access the text data from the form
-    console.log(req.body.location)
-    console.log(req.body.season)
+  // access the text data from the form
+  let location = req.body.location
+  let season = req.body.season
 
-    if (!file) {
-      const error = new Error('Please upload a file')
-      error.httpStatusCode = 400
-      return next(error)
-    }
-    processImage().then(output => {
-      res.setHeader('Content-Type', 'application/json');
-      res.json({ "price": `${output}`});
-    })
-
-  });
-
-  app.listen(5000,function(){
-    console.log("Started on PORT 5000");
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+  processImage(location, season).then(output => { //You must activate the python virtual environment or this will not work
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ "price": `${output}`});
   })
-  
+
+});
--- controller.py
+++ controller.py
@@ -2,17 +2,25 @@ import sys
 import pickle
 import numpy as np
 import math
+import ast
 
 
 #load the model
-with open('./price-predict/lin-reg.sav', "rb") as f:
+with open('./price-predict/neuralnet.sav', "rb") as f:
      loaded_model = pickle.load(f)
 
 #convert the data to float, as the model requires this
-arg_one = float(sys.argv[1])
+location = float(sys.argv[2])
+
+#convert string array to array of floats
+season = sys.argv[1]
+season = ast.literal_eval(season)       #this line specifically converts the string into a list
+seasons = [float(i) for i in season]    #this turns each item in the list into a float
 
 #create input
-data = [arg_one, 0, 0, 0, 7.5, 4.2, 0, 0, 0, 0, 1, 0]
+seasons.extend([8.21, 3.9, 0, 0, 0, 1, 0, 0, location])
+data = seasons
+# data = [arg_one, 0, 0, 0, 8.21, 3.9, 0, 0, 0, 1, 0, 0, location]
 data = np.reshape(data, (1, -1))
 
 #predict and round
