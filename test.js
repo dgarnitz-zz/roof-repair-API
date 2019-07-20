ef84c2fb8b4ec363fa483b7cdc8f041e4e110032
dag8 <dag8@pc5-018-l.cs.st-andrews.ac.uk>
2019-07-20 16:33:05 +0100

    added unit tests for helper functions

---
+++ test.js
@@ -0,0 +1,53 @@
+var rewire = require('rewire');
+var chai = require('chai');
+var should = chai.should();
+
+var app = rewire("./app.js")
+
+processLocation = app.__get__('processLocation')
+processSeason = app.__get__('processSeason')
+
+
+describe('helper functions', function () {
+
+    //check processLocation
+    describe('processLocation', function(){
+        it('should output the correct location vector', function(done){
+            processLocation("Fife").should.equal(1);
+            done();
+        });
+        it('should output the correct location vector', function(done){
+            processLocation("Tayside").should.equal(0);
+            done();
+        });
+        it('should output the correct location vector', function(done){
+            processLocation("other").should.equal(0);
+            done();
+        });
+    });
+
+    //check processSeason
+    describe('processSeason', function(){
+        it('should output the correct season vector', function(done){
+            processSeason("winter").should.eql([1,0,0,0]);
+            done();
+        });
+        it('should output the correct season vector', function(done){
+            processSeason("spring").should.eql([0,1,0,0]);
+            done();
+        });
+        it('should output the correct season vector', function(done){
+            processSeason("summer").should.eql([0,0,1,0]);
+            done();
+        });
+        it('should output the correct season vector', function(done){
+            processSeason("autumn").should.eql([0,0,0,1]);
+            done();
+        });
+        it('should output the correct season vector', function(done){
+            processSeason("other").should.eql([0,0,1,0]);
+            done();
+        });
+    });
+});
+
